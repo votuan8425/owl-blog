afa174a8ebc3e8f7c50a8bb3acad3271e9e284ab
votuan8425 <sgkcd1231@gmail.com>
2023-05-12 00:34:13 +0700

    feat: add prod

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,6 +24,7 @@ import { Context } from "./types/Context"
 import { buildDataLoaders } from "./utils/dataLoaders"
 
 const main = async () => {
+
     const connection = await createConnection({
         type: "postgres",
         database: "Reddit",
