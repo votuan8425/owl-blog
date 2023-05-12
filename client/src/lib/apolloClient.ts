1ed2cfca6839a1cf3e2c276f2a4b6d00cf09cdb8
votuan8425 <sgkcd1231@gmail.com>
2023-05-12 11:35:12 +0700

    fix: change domain be

--- client/src/lib/apolloClient.ts
+++ client/src/lib/apolloClient.ts
@@ -51,7 +51,7 @@ function createApolloClient(headers: IncomingHttpHeaders | null = null) {
 		// uri: 'http://localhost:4000/graphql', // Server URL (must be absolute)
 		uri:
 			process.env.NODE_ENV === 'production'
-				? 'https://sleepy-castle-81019.herokuapp.com/graphql'
+				? 'https://owl-blog-backend-votuan8425.vercel.app/graphql'
 				: 'http://localhost:4000/graphql',
 		credentials: 'include', // Additional fetch() options like `credentials` or `headers`
 		fetch: enhancedFetch
